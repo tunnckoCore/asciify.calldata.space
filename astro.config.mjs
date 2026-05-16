24754d45559fde6e5dd092a59872904e6e88d1df
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2026-05-16 06:13:59 +0300

    chore(deps): replace @astrojs/vercel with @astrojs/netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel";
+import netlify from "@astrojs/netlify";
 // import tunnel from "astro-tunnel";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [tailwind(), react()],
 });
